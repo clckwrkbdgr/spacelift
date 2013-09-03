4f971802e239e2ae8ff93ff25a16c589b8f155e3
Igor Chaika <kp580bm1@gmail.com>
2013-09-03 22:27:20 +0300

    Empty bonuses. Object types.

--- proto.py
+++ proto.py
@@ -2,6 +2,7 @@
 import sys
 import pygame
 import operator
+import random
 from pygame.locals import *
 
 BLACK = 0, 0, 0
@@ -9,6 +10,7 @@ BLUE = 0, 0, 255
 GRAY = 80, 80, 80
 WHITE = 255, 255, 255
 RED = 255, 0, 0
+YELLOW = 255, 255, 0
 
 SCREEN_SIZE = 640, 480
 LEVEL_SPEED = 0.3
@@ -25,15 +27,24 @@ BULLET_DAMAGE = 10
 PLAYER_SHOOTING_DELAY = 100
 ENEMY_SHOOTING_DELAY = 1000
 START_POS = 320, 420
+BONUS_SIZE = 32, 32
+
+PLATFORM_TYPE = PLATFORM_SIZE, BLUE, PLATFORM_HP, (0, 0)
+ENEMY_TYPE = ENEMY_SIZE, RED, ENEMY_HP, (0, ENEMY_SPEED + LEVEL_SPEED)
+BONUS_TYPE = BONUS_SIZE, YELLOW, 0, (0, LEVEL_SPEED)
+PLAYER_BULLET_TYPE = BULLET_SIZE, BLUE, 0, (0, -BULLET_SPEED)
+ENEMY_BULLET_TYPE = BULLET_SIZE, RED, 0, (0, BULLET_SPEED)
 
 class Object:
-	def __init__(self, size, color, pos, max_hp):
+	def __init__(self, pos, type_values):
+		size, color, self.max_hp, self.speed = type_values
+
 		self.surface = pygame.surface.Surface(size)
 		self.surface.fill(color)
+
 		self.pos = [float(x) for x in pos[:2]]
 		self.alive = True
-		self.max_hp = max_hp
-		self.hp = max_hp
+		self.hp = self.max_hp
 		self.shooting_delay = 0
 
 	def move(self, x, y, rect_list=None):
@@ -42,6 +53,9 @@ class Object:
 		if rect_list is not None:
 			if self.get_rect().collidelist(rect_list) != -1:
 				self.pos = old_pos
+
+	def auto_move(self):
+		self.move(self.speed[0], self.speed[1])
 
 	def get_rect(self):
 		rect = self.surface.get_rect()
@@ -64,16 +78,16 @@ wall_right.fill(GRAY)
 wall_right_rect = wall_right.get_rect()
 wall_right_rect.topright = SCREEN_SIZE[0], 0
 
-enemy_map = []
-for i in range(20):
-	enemy_map.append( (i * 100, 100 + i * 25) )
+enemy_map = [(i * 1000, 100 + i * 25) for i in range(20)]
+bonus_map = [(100 + i * 1000, random.randrange(wall_left_rect.right + BONUS_SIZE[0]/2, wall_right_rect.left - BONUS_SIZE[0]/2)) for i in range(5)]
 
-platform = Object(PLATFORM_SIZE, BLUE, START_POS, PLATFORM_HP)
+platform = Object(START_POS, PLATFORM_TYPE)
 platform_count = 3
+level_pos = 0
 enemies = []
 player_bullets = []
 enemy_bullets = []
-level_pos = 0
+bonuses = []
 
 while True:
 	for event in pygame.event.get():
@@ -87,52 +101,64 @@ while True:
 
 	# Logic.
 	level_pos += LEVEL_SPEED
+
 	for map_pos, screen_pos in enemy_map:
 		if map_pos < level_pos:
-			enemies.append(Object(ENEMY_SIZE, RED, (screen_pos, 0), ENEMY_HP))
+			enemies.append(Object((screen_pos, 0), ENEMY_TYPE))
 	enemy_map = [(map_pos, screen_pos) for map_pos, screen_pos in enemy_map if map_pos > level_pos]
 
-	if shooting and platform.shooting_delay <= 0:
-		player_bullets.append(Object(BULLET_SIZE, BLUE, platform.pos, 0))
-		platform.shooting_delay = PLAYER_SHOOTING_DELAY
-
-	if platform.shooting_delay > 0:
-		platform.shooting_delay -= 1
-
+	for map_pos, screen_pos in bonus_map:
+		if map_pos < level_pos:
+			bonuses.append(Object((screen_pos, 0), BONUS_TYPE))
+	bonus_map = [(map_pos, screen_pos) for map_pos, screen_pos in bonus_map if map_pos > level_pos]
 
 	platform.move(shift, 0, [wall_left_rect, wall_right_rect])
+	for bullet in player_bullets:
+		bullet.auto_move()
+	for bullet in enemy_bullets:
+		bullet.auto_move()
+	for bonus in bonuses:
+		bonus.auto_move()
+	for enemy in enemies:
+		enemy.auto_move()
+
 	for bullet in enemy_bullets:
 		if platform.get_rect().colliderect(bullet.get_rect()):
 			bullet.alive = False
 			platform.hp -= BULLET_DAMAGE
-	for bullet in player_bullets:
-		bullet.move(0, -BULLET_SPEED)
-	for bullet in enemy_bullets:
-		bullet.move(0, BULLET_SPEED)
 	for enemy in enemies:
-		enemy.move(0, ENEMY_SPEED + LEVEL_SPEED)
 		for bullet in player_bullets:
 			if enemy.get_rect().colliderect(bullet.get_rect()):
 				bullet.alive = False
 				enemy.hp -= BULLET_DAMAGE
+	for enemy in enemies:
 		if enemy.shooting_delay <= 0:
-			enemy_bullets.append(Object(BULLET_SIZE, RED, enemy.pos, 0))
+			enemy_bullets.append(Object(enemy.pos, ENEMY_BULLET_TYPE))
 			enemy.shooting_delay = ENEMY_SHOOTING_DELAY
 		if enemy.shooting_delay > 0:
 			enemy.shooting_delay -= 1
+	for enemy in enemies:
 		if enemy.get_rect().colliderect(platform.get_rect()):
 			enemy.alive = False
 			platform.hp -= ENEMY_DAMAGE
+	for enemy in enemies:
 		if enemy.hp < 0:
 			enemy.alive = False
+	if shooting and platform.shooting_delay <= 0:
+		player_bullets.append(Object(platform.pos, PLAYER_BULLET_TYPE))
+		platform.shooting_delay = PLAYER_SHOOTING_DELAY
+	if platform.shooting_delay > 0:
+		platform.shooting_delay -= 1
+	if platform.hp <= 0:
+		platform_count -= 1
+		platform = Object(START_POS, PLATFORM_TYPE)
+		if platform_count < 0:
+			sys.exit()
+
 	enemies = [enemy for enemy in enemies if enemy.alive and enemy.get_rect().top < SCREEN_SIZE[1]]
 	player_bullets = [bullet for bullet in player_bullets if bullet.alive and 0 < bullet.get_rect().bottom]
 	enemy_bullets = [bullet for bullet in enemy_bullets if bullet.alive and bullet.get_rect().top < SCREEN_SIZE[1]]
-	if platform.hp <= 0:
-		platform_count -= 1
-		platform = Object(PLATFORM_SIZE, BLUE, START_POS, PLATFORM_HP)
-		if platform_count < 0:
-			sys.exit()
+	bonuses = [bonus for bonus in bonuses if bonus.alive and bonus.get_rect().top < SCREEN_SIZE[1]]
 
 	# Drawing.
 	screen.fill(BLACK)
@@ -145,11 +171,17 @@ while True:
 		screen.blit(enemy.surface, enemy.get_rect())
 	for bullet in enemy_bullets:
 		screen.blit(bullet.surface, bullet.get_rect())
+	for bonus in bonuses:
+		screen.blit(bonus.surface, bonus.get_rect())
 
 	hud = ["You: ({0:10.2f},{1:10.2f}) {2}/{3}hp x{4}".format(platform.pos[0], platform.pos[1], platform.hp, platform.max_hp, platform_count)]
 	hud.append("Level: {0:10.2f}; {1}".format(level_pos, "shooting" if shooting else ""))
+	for bonus in bonuses:
+		hud.append("Bonus: {0:10.2f}, {1:#10.2f}".format(bonus.pos[0], bonus.pos[1]))
 	for enemy in enemies:
 		hud.append("Enemy: {0:10.2f}, {1:#10.2f}".format(enemy.pos[0], enemy.pos[1]))
+	for bullet in enemy_bullets:
+		hud.append("Bullet: {0:10.2f}, {1:#10.2f}".format(bullet.pos[0], bullet.pos[1]))
 	for bullet in player_bullets:
 		hud.append("Bullet: {0:10.2f}, {1:#10.2f}".format(bullet.pos[0], bullet.pos[1]))
 	for number, line in enumerate(hud):

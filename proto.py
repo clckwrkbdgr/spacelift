288c1b9900b4314bf10985f1cab1472e64db85ed
Igor Chaika <kp580bm1@gmail.com>
2013-09-06 22:41:59 +0300

    Controllers. Generic behaviour for all objects. Generic level map. One general object list.

--- proto.py
+++ proto.py
@@ -40,7 +40,6 @@ COLLIDES.append((ENEMY_PARTY, PLAYER_BULLET_PARTY))
 COLLIDES.append((ENEMY_PARTY, PLAYER_PARTY))
 COLLIDES.append((ENEMY_BULLET_PARTY, PLAYER_PARTY))
 COLLIDES.append((BONUS_PARTY, PLAYER_PARTY))
-collides = lambda a, b: ((a, b) in COLLIDES) or ((b, a) in COLLIDES) # TODO check if works
 
 # TYPE             = PARTY                SIZE           COLOR   MAX_HP       DAMAGE           SPEED                           SHOOTING DELAY         BULLET_TYPE
 BONUS_TYPE         = BONUS_PARTY,         BONUS_SIZE   , YELLOW, 1,           0,               (0, LEVEL_SPEED),               None,                  None
@@ -50,8 +49,9 @@ PLATFORM_TYPE      = PLAYER_PARTY,        PLATFORM_SIZE, BLUE  , PLATFORM_HP, PL
 ENEMY_TYPE         = ENEMY_PARTY,         ENEMY_SIZE   , RED   , ENEMY_HP,    ENEMY_DAMAGE,    (0, ENEMY_SPEED + LEVEL_SPEED), ENEMY_SHOOTING_DELAY,  ENEMY_BULLET_TYPE
 
 class Object:
-	def __init__(self, pos, type_values):
+	def __init__(self, pos, type_values, controller=None):
 		self.party, size, color, self.max_hp, self.damage, self.speed, self.max_shooting_delay, self.bullet_type = type_values
+		self.controller = controller
 
 		self.surface = pygame.surface.Surface(size)
 		self.surface.fill(color)
@@ -85,6 +85,26 @@ class Object:
 		rect.center = tuple(map(int, self.pos))
 		return rect
 
+class Player:
+	def __init__(self, rects):
+		self.rects = rects
+		self.shift = (0, 0)
+		self.shooting = False
+
+	def move(self, o):
+		o.move(self.shift, 0, self.rects)
+
+	def shoot(self, o):
+		return o.shoot(self.shooting)
+
+class Enemy:
+	def move(self, o):
+		pass
+
+	def shoot(self, o):
+		return o.shoot(True)
+
+
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.mouse.set_visible(False)
@@ -101,16 +121,16 @@ wall_right.fill(GRAY)
 wall_right_rect = wall_right.get_rect()
 wall_right_rect.topright = SCREEN_SIZE[0], 0
 
-enemy_map = [(i * 500, 100 + i * 25) for i in range(20)]
-bonus_map = [(100 + i * 1000, random.randrange(wall_left_rect.right + BONUS_SIZE[0]/2, wall_right_rect.left - BONUS_SIZE[0]/2)) for i in range(5)]
+level_map = []
+level_map += [(ENEMY_TYPE, Enemy, i * 500, 100 + i * 25) for i in range(20)]
+level_map += [(BONUS_TYPE, None, 100 + i * 1000, random.randrange(wall_left_rect.right + BONUS_SIZE[0]/2, wall_right_rect.left - BONUS_SIZE[0]/2)) for i in range(5)]
 
-platform = Object(START_POS, PLATFORM_TYPE)
+player = Player([wall_left_rect, wall_right_rect])
+
 platform_count = 3
 level_pos = 0
-enemies = []
-player_bullets = []
-enemy_bullets = []
-bonuses = []
+platform = Object(START_POS, PLATFORM_TYPE, player)
+objects = [platform]
 
 while True:
 	for event in pygame.event.get():
@@ -119,48 +139,41 @@ while True:
 		elif event.type == pygame.KEYDOWN:
 			if event.key == K_q or event.key == K_ESCAPE:
 				sys.exit()
-	shift = pygame.mouse.get_rel()[0]
-	shooting = any(pygame.mouse.get_pressed())
+	player.shift = pygame.mouse.get_rel()[0]
+	player.shooting = any(pygame.mouse.get_pressed())
 
 	# Logic.
 	level_pos += LEVEL_SPEED
 
-	for map_pos, screen_pos in enemy_map:
+	for object_type, controller, map_pos, screen_pos in level_map:
 		if map_pos < level_pos:
-			enemies.append(Object((screen_pos, 0), ENEMY_TYPE))
-	enemy_map = [(map_pos, screen_pos) for map_pos, screen_pos in enemy_map if map_pos > level_pos]
+			objects.append(Object((screen_pos, 0), object_type, controller() if controller else None))
+	level_map = [(object_type, controller, map_pos, screen_pos) for object_type, controller, map_pos, screen_pos in level_map if map_pos > level_pos]
 
-	for map_pos, screen_pos in bonus_map:
-		if map_pos < level_pos:
-			bonuses.append(Object((screen_pos, 0), BONUS_TYPE))
-	bonus_map = [(map_pos, screen_pos) for map_pos, screen_pos in bonus_map if map_pos > level_pos]
-
-	platform.move(shift, 0, [wall_left_rect, wall_right_rect])
-	for o in player_bullets + enemy_bullets + bonuses + enemies:
+	new_objects = []
+	for o in objects:
+		if o.controller:
+			o.controller.move(o)
+			new_objects += o.controller.shoot(o)
 		o.auto_move()
+	objects += new_objects
 
-	for enemy in enemies:
-		enemy_bullets += enemy.shoot(True)
-	player_bullets += platform.shoot(shooting)
-
-	for a in [platform] + enemies + player_bullets + enemy_bullets + bonuses:
-		for b in [platform] + enemies + player_bullets + enemy_bullets + bonuses:
-			if collides(a.party, b.party):
+	for a in objects:
+		for b in objects:
+			if (a.party, b.party) in COLLIDES:
 				if a.get_rect().colliderect(b.get_rect()):
 					a.hp -= b.damage
 					b.hp -= a.damage
 
-	for o in [platform] + enemies + player_bullets + enemy_bullets + bonuses:
+	for o in objects:
 		if o.max_hp > 0 and o.hp <= 0:
 			o.alive = False
 
-	enemies = [enemy for enemy in enemies if enemy.alive and enemy.get_rect().top < SCREEN_SIZE[1]]
-	player_bullets = [bullet for bullet in player_bullets if bullet.alive and 0 < bullet.get_rect().bottom]
-	enemy_bullets = [bullet for bullet in enemy_bullets if bullet.alive and bullet.get_rect().top < SCREEN_SIZE[1]]
-	bonuses = [bonus for bonus in bonuses if bonus.alive and bonus.get_rect().top < SCREEN_SIZE[1]]
+	objects = [o for o in objects if o.alive and 0 < o.get_rect().bottom and o.get_rect().top < SCREEN_SIZE[1]]
 	if not platform.alive:
 		platform_count -= 1
-		platform = Object(START_POS, PLATFORM_TYPE)
+		platform = Object(START_POS, PLATFORM_TYPE, player)
+		objects.append(platform)
 		if platform_count < 0:
 			sys.exit()
 
@@ -168,11 +181,11 @@ while True:
 	screen.fill(BLACK)
 	screen.blit(wall_left, wall_left_rect)
 	screen.blit(wall_right, wall_right_rect)
-	for o in [platform] + bonuses + enemies + enemy_bullets + player_bullets:
+	for o in objects:
 		screen.blit(o.surface, o.get_rect())
 
-	hud = ["Level: {0:10.2f}; x{2}, {1}".format(level_pos, "shooting" if shooting else "", platform_count)]
-	for o in [platform] + bonuses + enemies + enemy_bullets + player_bullets:
+	hud = ["Level: {0:10.2f}; x{2}, {1}".format(level_pos, "shooting" if player.shooting else "", platform_count)]
+	for o in objects:
 		hud.append("{0}: ({1:0.2f},{2:#0.2f}) {3}/{4}hp".format(o.party, o.pos[0], o.pos[1], o.hp, o.max_hp))
 	for number, line in enumerate(hud):
 		screen.blit(font.render(line, True, WHITE), (wall_left_rect.right, number * font.get_height()))
